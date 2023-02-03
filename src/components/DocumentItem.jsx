adbe2b0ef57849a7fe427df591babf0381e9b7a3
Carlos Valencia <valenciacarlos278@gmail.com>
2023-02-03 13:34:04 -0500

    Bibliography template added with conditional rendering

--- src/components/DocumentItem.jsx
+++ src/components/DocumentItem.jsx
@@ -1,15 +1,26 @@
-import '../styles/DocumentItem.css'
+import "../styles/DocumentItem.css";
 
-const DocumentItem = ({document}) => {
-
-    return (
-        <div className="card">
-            <img src={document.img} alt="Imagen del documento" className='card-image'/>
-            <h2 className='card-title'>{document.title}</h2>
-            <p className='card-year'>Año {document.year}</p>
-            <p className='card-description'>{document.description}</p>
-        </div>
-    );
-}
+const DocumentItem = ({ document, childToParent }) => {
+  return (
+    // <div className="card">
+    //     <img src={document.img} alt="Imagen del documento" className='card-image'/>
+    //     <h2 className='card-title'>{document.title}</h2>
+    //     <p className='card-year'>Año {document.year}</p>
+    //     <p className='card-description'>{document.description}</p>
+    // </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img src={document.img} className="card-img-top" alt="..." />
+      <div className="card-body d-flex flex-column justify-content-between">
+        <h5 className="card-title">{document.title}</h5>
+        <p className="card-text">
+          {document.description}
+        </p>
+        <button className="btn btn-primary" onClick={() => childToParent(document)}>
+            Info
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default DocumentItem;
--- src/containers/UserDashboard.jsx
+++ src/containers/UserDashboard.jsx
@@ -1,35 +1,41 @@
 import { useState, useRef } from "react";
 import DashboardHeader from "../components/DashboardHeader";
 import DocumentItem from "../components/DocumentItem";
+import DocumentBibliography from "../components/DocumentBibliography";
 import '../styles/UserDashboard.css'
 
 const items = [
     {
         id: 1,
+        type: 'document',
         title: 'Censo Funza 1982',
         description: 'Libro con el censo de Funza del año 1982',
         year: 1982,
-        img: '/public/documentImages/1.jpg'
+        img: 'https://drive.google.com/uc?export=view&id=1PYuKNt0PJXAZxSeljFrpBTHeIkAeLh7q'
     },
     {
         id: 2,
+        type: 'document',
         title: 'Registro de natalicios 50\'s ',
         description: 'Registros de natalicios de la década de los 50',
         year: 1982,
-        img: 'public/documentImages/2.jpg'
+        img: 'https://drive.google.com/uc?export=view&id=1eicF6KcQZVeCNkCGxUO5rI2Rlzs2ixLY'
     },
     {
         id: 3,
+        type: 'document',
         title: 'Profe rarito',
         description: 'Pues eso, un profe rarito',
         year: 2022,
-        img: 'public/documentImages/3.jpg'
+        img: 'https://drive.google.com/uc?export=view&id=1kDsq0hb5du3G0utUjKxUAXFikz-U-CYY'
     }
 ]
 
 const UserDashboard = () => {
 
     const [itemsToRender, setItemsToRender] = useState([])
+    const [bibliography, setBibliography] = useState({})
+    const [renderBibliography, setRenderBibliography] = useState(false)
     const searchTerm = useRef()
 
     const searchButton = () => {
@@ -37,8 +43,15 @@ const UserDashboard = () => {
         const searchTermValue = searchTerm.current.value.toLocaleLowerCase()
         const queryItems = items.filter((item) => item.title.toLocaleLowerCase().includes(searchTermValue))
         setItemsToRender(queryItems)
+        setRenderBibliography(false)
         searchTerm.current.value = ''
 
+    }
+
+    const renderDocument = (documentFromChild) => {
+        console.log('event triggered')
+        setRenderBibliography(true)
+        setItemsToRender([])
     }
 
     return (
@@ -51,9 +64,13 @@ const UserDashboard = () => {
             </div>
             <div className="items-container">
                 {itemsToRender.map((item) => {
-                    return <DocumentItem document={item} key={item.id} />
+                    return <DocumentItem document={item} key={item.id} childToParent={renderDocument} />
                 })}
             </div>
+            <div className="container-bib">
+                {renderBibliography && <DocumentBibliography />}
+            </div>
+            
             
         </div>
         </>
