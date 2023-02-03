7f9ecebf1cca7a3b06c342586018c120749d1c38
Carlos Valencia <valenciacarlos278@gmail.com>
2023-02-03 11:32:17 -0500

    Navbar modified to use bootstrap on dashboard component

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,15 +1,27 @@
+import { Link } from 'react-router-dom'
 import "../styles/DashboardHeader.css"
 import profilePic from "../assets/person.png"
 const DashboardHeader = () => {
     return (
         <header>
-            <nav>
-                <p className="logo" >App Patrimonio</p>
-                <div className="profile-info">
-                    <img src={profilePic} alt="profile pic" className="profile-pic"/>
-                    <p>Hola, Juan Pablo</p>
+            <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand" href="/dashboard">App Patrimonio</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/dashboard" className="nav-link active" aria-current="page" href="#">Inicio</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/dashboard" className="nav-link" href="#">Marcadores</Link>
+                        </li>
+                    </ul>
+                    </div>
                 </div>
-            </nav>
+                </nav>
         </header>
     );
 }
