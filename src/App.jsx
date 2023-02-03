a04215536f8fadbffda889b2446978030b0c0c12
Carlos Valencia <valenciacarlos278@gmail.com>
2023-02-02 21:40:09 -0500

    Initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './containers/Layout'
+import Login from './components/Login'
+import UserDashboard from './containers/UserDashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Layout />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/dashboard' element={<UserDashboard />} />
+        <Route path='*' element={<Layout />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
